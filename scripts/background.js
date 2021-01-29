47c0958006f11c2d9e3ae34d0231b2c0d9f66ab7
cheerypal <ejg9@hw.ac.uk>
2021-01-29 14:03:28 +0000

    options page stated

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,6 @@
 /* This will be background scripts that will create functionality for the adblocker */
 
-/* Set up badge for showing how many ad have been blocked */
-
+/* Set up badge for showing how many ads have been blocked */
 chrome.browserAction.setBadgeBackgroundColor({ color: "#161616" });
 
 /* init check for any local storage */
