471f94b4699d58d5cb37c5f53b810a014b3cc722
cheerypal <ejg9@hw.ac.uk>
2021-02-07 18:48:38 +0000

    ad server request blocked + on/off

--- filters/alterhosts.py
+++ filters/alterhosts.py
@@ -1,22 +1,24 @@
 # create a JSON array of hosts that should be blocked.
-
-import json
 # format the lines of the file so that they are easily read by the chrome api
+import json
+
+# format line
 def format(x):
-    x = x.replace('||', '*://*.')
-    x = x.replace('^', "/*")
-    x = x.replace("\n", "")
+    x = x.replace('127.0.0.1 ', '*://*.')
+    x = x.replace('\n', "/*")
     return x
 
 
-# open the file and read all the lines excluding the first 27 lines. 
+# open the file and read all the lines excluding the first 14 lines. These lines are for the author or the list
 with open("hosts.txt", "r") as f:
     lines = f.readlines()
-    lines = lines[27:]
+    lines = lines[14:]
     
     # format each line in the file 
     formated_line = [format(line) for line in lines]
-    # create a new file for the altered hosts
-    with open("alteredHosts.json", "w") as nFile:
-        nFile.write(json.dumps(formated_line))
+    # create a new js file for the altered hosts
+    with open("alteredHosts.js", "w") as nFile:
+        nFile.write("const hosts = {hosts:")
+        nFile.write(str(formated_line))
+        nFile.write("};")
 
--- scripts/requestBlocker.js
+++ scripts/requestBlocker.js
@@ -2,15 +2,12 @@
 /* Ad-requests and sketchy sites will be blocked here */
 
 if (typeof Storage !== "undefined") {
-  if (localStorage.BLKState) {
-    if (localStorage.BLKState === "On") {
-      chrome.webRequest.onBeforeRequest.addListener(
-        function () {
-          return { cancel: true };
-        },
-        { urls: ["*://*.doubleclick.net/*"] },
-        ["blocking"]
-      );
-    }
-  }
+  chrome.webRequest.onBeforeRequest.addListener(
+    function () {
+      if (localStorage.BLKState === "On") return { cancel: true };
+      else return { cancel: false };
+    },
+    { urls: hosts["hosts"] },
+    ["blocking"]
+  );
 }
